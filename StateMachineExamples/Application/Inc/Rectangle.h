f6cc54d33b930d143e0675b898967e1a4c650a80
FurkanAltay61 <110613102+FurkanAltay61@users.noreply.github.com>
2022-08-19 09:42:27 +0300

    Rectangle Class is added

--- StateMachineExamples/Application/Inc/Rectangle.h
+++ StateMachineExamples/Application/Inc/Rectangle.h
@@ -14,12 +14,13 @@
  * Structure Definition
  */
 /*********************************************************************
- * @fn      		  - Shape Class
+ * @fn      		  - Rectangle Class
  *
- * @brief             -
+ * @brief             - Class that contains basic rectangle member elements
  *
- * @param[in]         - x - coordinate of Shape's position
- * @param[in]         - y - coordinate of Shape's position
+ * @param[super]      - Shape super structure contains shape's elements
+ * @param[width]      - Rectangle shape width elements
+ * @param[height]      - Rectangle shape height elements
  *
  * @Note              -
 
@@ -34,11 +35,58 @@ typedef struct{
 }Rectangle;
 
 /* Rectangle's operations.. */
+
+
+/*********************************************************************
+ * @fn      		  - Rectangle_ctor
+ *
+ * @brief             - Rectangle constructor function
+ *
+ * @param[me]         - Rectangle pointer structure
+ * @param[x0]         - x-Coordinate of Shape Structure
+ * @param[y0]         - y-Coordinate of Shape Structure
+ * @param[w0]         - Width of Rectangle Structure
+ * @param[h0]         - Height of Rectangle Structure
+ *
+ * @return			  - void
+ *
+ * @Note              - This function initialises rectangle structure member elements
+
+ */
 void Rectangle_ctor(Rectangle * const me,
 					int16_t x0,int16_t y0,
 					uint16_t w0,uint16_t h0);
 
+
+
+/*********************************************************************
+ * @fn      		  - Rectangle_draw
+ *
+ * @brief             - Rectangle draw function
+ *
+ * @param[me]         - Rectangle pointer structure
+ *
+ * @return			  - void
+ *
+ * @Note              - This function draws a rectangle based on width and height members
+
+ */
 void Rectangle_draw(Rectangle const * const me);
+
+
+/*********************************************************************
+ * @fn      		  - Rectangle_area
+ *
+ * @brief             - Rectangle area calculator
+ *
+ * @param[me]         - Rectangle pointer structure
+ *
+ * @return			  - Rectangle area absolute value
+ *
+ * @Note              - This function calculates a rectangle area based on width and height members
+
+ */
+uint32_t Rectangle_area(Rectangle const * const me);
 
 
 #endif /* INC_RECTANGLE_H_ */
--- StateMachineExamples/Application/Src/Rectangle.c
+++ StateMachineExamples/Application/Src/Rectangle.c
@@ -6,3 +6,70 @@
  */
 
 
+#include "Rectangle.h"		/* Rectangle class interface */
+
+
+
+
+/*********************************************************************
+ * @fn      		  - Rectangle_ctor
+ *
+ * @brief             - Rectangle constructor function
+ *
+ * @param[me]         - Rectangle pointer structure
+ * @param[x0]         - x-Coordinate of Shape Structure
+ * @param[y0]         - y-Coordinate of Shape Structure
+ * @param[w0]         - Width of Rectangle Structure
+ * @param[h0]         - Height of Rectangle Structure
+ *
+ * @return			  - void
+ *
+ * @Note              - This function initialises rectangle structure member elements
+
+ */
+void Rectangle_ctor(Rectangle * const me,
+					int16_t x0,int16_t y0,
+					uint16_t w0,uint16_t h0)
+{
+
+
+}
+
+
+
+/*********************************************************************
+ * @fn      		  - Rectangle_draw
+ *
+ * @brief             - Rectangle draw function
+ *
+ * @param[me]         - Rectangle pointer structure
+ *
+ * @return			  - void
+ *
+ * @Note              - This function draws a rectangle based on width and height members
+
+ */
+void Rectangle_draw(Rectangle const * const me){
+
+}
+
+
+/*********************************************************************
+ * @fn      		  - Rectangle_area
+ *
+ * @brief             - Rectangle area calculator
+ *
+ * @param[me]         - Rectangle pointer structure
+ *
+ * @return			  - Rectangle area absolute value
+ *
+ * @Note              - This function calculates a rectangle area based on width and height members
+
+ */
+uint32_t Rectangle_area(Rectangle const * const me){
+
+}
+
+
+
+
