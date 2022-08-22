60e2f26265ae9eec5ac41d7188b04232f5333928
FurkanAltay61 <110613102+FurkanAltay61@users.noreply.github.com>
2022-08-22 10:59:30 +0300

    garbage debug files

---
+++ StateMachineExamples/Debug/Application/Src/Shape.d
@@ -0,0 +1,3 @@
+Application/Src/Shape.o: ../Application/Src/Shape.c \
+ C:/Users/furkan.altay/Documents/GitHub/desktop-tutorial/StateMachineExamples/Application/Inc/Shape.h
+C:/Users/furkan.altay/Documents/GitHub/desktop-tutorial/StateMachineExamples/Application/Inc/Shape.h:
