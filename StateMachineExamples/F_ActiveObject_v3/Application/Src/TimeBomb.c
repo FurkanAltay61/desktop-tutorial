f26d5295c269277a008bbe9387841da0f7da0b9f
FurkanAltay61 <110613102+FurkanAltay61@users.noreply.github.com>
2022-08-31 11:53:10 +0300

    TimeBomb Dispatch Function Modified
    Timebomb dispatch , ctor functions are modified according to Timebomb State Machine

--- StateMachineExamples/F_ActiveObject_v3/Application/Src/TimeBomb.c
+++ StateMachineExamples/F_ActiveObject_v3/Application/Src/TimeBomb.c
@@ -27,7 +27,6 @@
 void TimeBomb_ctor(TimeBomb * const me) {
     Active_ctor(&me->super, (DispatchHandler)&TimeBomb_dispatch); /*Initiliase the active object structure */
     TimeEvent_ctor(&me->te,TIMEOUT_SIG,&me->super);				  /*Initiliase the TimeoutEvent structure */
-    me->blink_time = INITIAL_BLINK_TIME;					      /*Led blink time period is chosen */
 }
 
 
@@ -51,77 +50,78 @@ void TimeBomb_dispatch(TimeBomb * const me, Event const * const e){
 
 
 	if(e->sig == INIT_SIG){
-		BspLedOff12();
-		TimeEvent_arm(&me->te, me->blink_time * 3U, 0U);
-		me->state = OFF_STATE;
-		return;
+		BspLedOn12();
+		me->state = WAIT4BUTTON_STATE;
 	}
 
 
 	switch(me->state){
 
-		case OFF_STATE:{
-			switch(e->sig){
+		case WAIT4BUTTON_STATE:{
 
-				case TIMEOUT_SIG:{
-					BspLedOn13();
-					TimeEvent_arm(&me->te, me->blink_time, 0U);
-					me->state = ON_STATE;
-					break;
-				}
+			switch(e->sig){
 
 				case BUTTON_PRESSED_SIG:{
 
-					BspLedOn12();
-
-					me->blink_time >>= 1; /*shorten the blink time by factor of 2*/
-					if(me->blink_time == 0){
-						me->blink_time = INITIAL_BLINK_TIME;
-					}
-					break;
-				}
-
-				case BUTTON_RELEASED_SIG:{
 					BspLedOff12();
-
+					BspLedOn13();
+					TimeEvent_arm(&me->te,OS_TICKS_PER_SEC/2, 0U);
+					me->blink_cntr = 3;
+					me->state = BLINK_STATE;
 					break;
+
 				}
 
-				break;
 			}
+
 			break;
 		}
 
-		case ON_STATE:{
+		case BLINK_STATE:{
 
 			switch(e->sig){
 
 				case TIMEOUT_SIG:{
 					BspLedOff13();
-					TimeEvent_arm(&me->te,me->blink_time * 3U, 0U);
-					me->state = OFF_STATE;
+					TimeEvent_arm(&me->te,OS_TICKS_PER_SEC/2, 0U);
+					me->state = PAUSE_STATE;
 					break;
 				}
 
-				case BUTTON_PRESSED_SIG:{
-
-					BspLedOn13();
-
-					me->blink_time >>= 1; /*shorten the blink time by factor of 2*/
-					if(me->blink_time == 0){
-						me->blink_time = INITIAL_BLINK_TIME;
-					}
-					break;
-				}
-
-				case BUTTON_RELEASED_SIG:{
-					BspLedOff13();
-					break;
-				}
-
-				break;
 			}
 
+			break;
+		}
+
+		case PAUSE_STATE:{
+
+			switch(e->sig){
+
+				case TIMEOUT_SIG:{
+					--me->blink_cntr;
+					if(me->blink_cntr > 0U){
+						BspLedOn13();
+						TimeEvent_arm(&me->te,OS_TICKS_PER_SEC/2, 0U);
+						me->state = BLINK_STATE;
+					}
+					else{
+						BspLedOn12();
+						BspLedOn13();
+						BspLedOn14();
+						BspLedOn15();
+						me->state = BOOM_STATE;
+					}
+
+					break;
+				}
+
+			}
+
+			break;
+		}
+
+
+		case BOOM_STATE:{
 			break;
 		}
 
