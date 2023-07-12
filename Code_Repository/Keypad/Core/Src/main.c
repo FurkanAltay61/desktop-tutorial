60ef873380785b2949c140a25713ea6f3953429d
FurkanAltay61 <frknalty61@gmail.com>
2023-07-12 11:48:38 +0300

    DMA prototype not working

--- Code_Repository/Keypad/Core/Src/main.c
+++ Code_Repository/Keypad/Core/Src/main.c
@@ -48,6 +48,8 @@ Q_DEFINE_THIS_FILE
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+DMA_HandleTypeDef hdma_i2c1_tx;
+DMA_HandleTypeDef hdma_i2c1_rx;
 
 /* USER CODE BEGIN PV */
 mpu6050 myMpu;
@@ -57,6 +59,7 @@ MPU6050_t MPU6050;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 void xITM_SendChar(uint8_t ch)
@@ -120,6 +123,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
   mpu6050_detect(&hi2c1);
@@ -232,6 +236,25 @@ static void MX_I2C1_Init(void)
   /* USER CODE BEGIN I2C1_Init 2 */
 
   /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream0_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream0_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream0_IRQn);
+  /* DMA1_Stream6_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
 
 }
 
--- Code_Repository/Keypad/Core/Src/my_mpu6050.c
+++ Code_Repository/Keypad/Core/Src/my_mpu6050.c
@@ -17,11 +17,15 @@ const float RAD_TO_DEG =  180/M_PI;
 uint32_t Prevtime,CurrTime;
 float elapsedTime;
 
+uint8_t txtemp=0,rxtemp=0;
+
+extern DMA_HandleTypeDef hdma_i2c1_rx;
+
 void mpu6050_detect(I2C_HandleTypeDef *h2cx){
 	uint8_t ret_val=0;
 	//Check is mpu6050 detect by constantly reading WHO_AM_I register
 	while(ret_val != 104){
-		HAL_I2C_Mem_Read(h2cx, MPU6050_HWADDR, WHO_AM_I_REG,1,&ret_val,1,100);
+		HAL_I2C_Mem_Read_DMA(h2cx, MPU6050_HWADDR, WHO_AM_I_REG,1,&ret_val,1);
 		HAL_Delay(10);
 	}
 }
@@ -29,20 +33,33 @@ void mpu6050_detect(I2C_HandleTypeDef *h2cx){
 uint8_t mpu6050_config(I2C_HandleTypeDef *h2cx){
 
 	uint8_t temp_val = 0;
+
+	txtemp = 0;
+
 	//1. Configure power management register
-	if(HAL_I2C_Mem_Write(h2cx, MPU6050_HWADDR, PWR_MGMT_1_REG,1,&temp_val,1,10) != HAL_OK) return 1;
+	HAL_I2C_Mem_Write_DMA(h2cx, MPU6050_HWADDR, PWR_MGMT_1_REG,1,&temp_val,1);
+
+	HAL_Delay(10);
 
 	//2. Configure sample rate divider
 	temp_val = 0x07;
-	if(HAL_I2C_Mem_Write(h2cx, MPU6050_HWADDR, SMPLRT_DIV_REG,1,&temp_val,1,10) != HAL_OK) return 1;
+	HAL_I2C_Mem_Write_DMA(h2cx, MPU6050_HWADDR, SMPLRT_DIV_REG,1,&temp_val,1);
+
+	HAL_Delay(10);
 
 	//3. Configure Gyroscope Register
 	temp_val = 0x10;
-	if(HAL_I2C_Mem_Write(h2cx, MPU6050_HWADDR, GYRO_CONFIG_REG,1,&temp_val,1,10) != HAL_OK) return 1;
+	HAL_I2C_Mem_Write_DMA(h2cx, MPU6050_HWADDR, GYRO_CONFIG_REG,1,&temp_val,1);
+
+	HAL_Delay(10);
 
 	//4. Configure Accelerometer Register
 	temp_val = 0x10;
-	if(HAL_I2C_Mem_Write(h2cx, MPU6050_HWADDR, ACCEL_CONFIG_REG,1,&temp_val,1,10) != HAL_OK) return 1;
+	HAL_I2C_Mem_Write_DMA(h2cx, MPU6050_HWADDR, ACCEL_CONFIG_REG,1,&temp_val,1);
+
+	HAL_Delay(10);
+
+	if(txtemp != 0x0F) return 1;
 
 	CurrTime = HAL_GetTick();
 
@@ -110,11 +127,16 @@ void mpu6050_ReadGyro(I2C_HandleTypeDef *h2cx, mpu6050 *xmpu){
 	//	 so we can read 6 byte from starting first register(ACCEL_XOUT_H)
 	uint8_t GyroOut[6]={0};
 
-	HAL_I2C_Mem_Read(h2cx, MPU6050_HWADDR, GYRO_XOUT_H_REG,1,GyroOut,6,100);
+	if(rxtemp == 0x01){
+		xmpu->RawGyro[X] = ((int16_t)(GyroOut[0] << 8 | GyroOut[1]))/32.8;
+		xmpu->RawGyro[Y] = ((int16_t)(GyroOut[2] << 8 | GyroOut[3]))/32.8;
+		xmpu->RawGyro[Z] = ((int16_t)(GyroOut[4] << 8 | GyroOut[5]))/32.8;
+	}
 
-	xmpu->RawGyro[X] = ((int16_t)(GyroOut[0] << 8 | GyroOut[1]))/32.8;
-	xmpu->RawGyro[Y] = ((int16_t)(GyroOut[2] << 8 | GyroOut[3]))/32.8;
-	xmpu->RawGyro[Z] = ((int16_t)(GyroOut[4] << 8 | GyroOut[5]))/32.8;
+	rxtemp &= (1 << 0);
+
+	HAL_I2C_Mem_Read_DMA(h2cx, MPU6050_HWADDR, GYRO_XOUT_H_REG,1,GyroOut,6);
+
 
 }
 
@@ -124,12 +146,15 @@ void mpu6050_ReadAccel(I2C_HandleTypeDef *h2cx, mpu6050 *xmpu){
 
 	uint8_t AccelOut[6]={0};
 
-	HAL_I2C_Mem_Read(h2cx, MPU6050_HWADDR, ACCEL_XOUT_H_REG,1,AccelOut,6,100);
+	if(rxtemp == 0x02){
+		xmpu->RawAccel[X] = ((int16_t)(AccelOut[0] << 8 | AccelOut[1])) / 4096.0;
+		xmpu->RawAccel[Y] = ((int16_t)(AccelOut[2] << 8 | AccelOut[3])) / 4096.0;
+		xmpu->RawAccel[Z] = ((int16_t)(AccelOut[4] << 8 | AccelOut[5])) / 4096.0;
+	}
 
-	xmpu->RawAccel[X] = ((int16_t)(AccelOut[0] << 8 | AccelOut[1])) / 4096.0;
-	xmpu->RawAccel[Y] = ((int16_t)(AccelOut[2] << 8 | AccelOut[3])) / 4096.0;
-	xmpu->RawAccel[Z] = ((int16_t)(AccelOut[4] << 8 | AccelOut[5])) / 4096.0;
+	rxtemp &= (1 << 1);
 
+	HAL_I2C_Mem_Read_DMA(h2cx, MPU6050_HWADDR, ACCEL_XOUT_H_REG,1,AccelOut,6);
 
 }
 
@@ -161,4 +186,56 @@ void mpu6050_CalcAngle(I2C_HandleTypeDef *h2cx, mpu6050 *xmpu){
 	xmpu->TotalAngleY = 0.98 * (xmpu->TotalAngleY + xmpu->GyroAngle[Y]) + 0.02 * xmpu->AccelAngle[Y];
 
 
+}
+
+
+/**
+  * @brief  Memory Tx Transfer completed callback.
+  * @param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
+  *                the configuration information for the specified I2C.
+  * @retval None
+  */
+void HAL_I2C_MemTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(hi2c);
+
+  /* NOTE : This function should not be modified, when the callback is needed,
+            the HAL_I2C_MemTxCpltCallback could be implemented in the user file
+   */
+  if(hi2c->Memaddress == PWR_MGMT_1_REG){
+	  txtemp |= 1 << 0;
+  }else if(hi2c->Memaddress == SMPLRT_DIV_REG){
+	  txtemp |= 1 << 1;
+  }else if(hi2c->Memaddress == GYRO_CONFIG_REG){
+	  txtemp |= 1 << 2;
+  }else if(hi2c->Memaddress == ACCEL_CONFIG_REG){
+	  txtemp |= 1 << 3;
+  }
+}
+
+/**
+  * @brief  Memory Rx Transfer completed callback.
+  * @param  hi2c Pointer to a I2C_HandleTypeDef structure that contains
+  *                the configuration information for the specified I2C.
+  * @retval None
+  */
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+  /* Prevent unused argument(s) compilation warning */
+  UNUSED(hi2c);
+
+  /* NOTE : This function should not be modified, when the callback is needed,
+            the HAL_I2C_MemRxCpltCallback could be implemented in the user file
+   */
+ if(hi2c->Instance == I2C1){
+  if(hi2c->Memaddress == GYRO_XOUT_H_REG){
+
+	  rxtemp |= 1 << 0;
+
+  }else if(hi2c->Memaddress == ACCEL_XOUT_H_REG){
+
+	  rxtemp |= 1 << 1;
+  }
+ }
 }
